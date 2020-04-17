10c651c91c85188819014b1327c90667c23ae99e
Eugenio Lacuesta <eugenio.lacuesta@gmail.com>
2020-04-17 10:46:19 -0300

    Test coroutine to deferred conversion

---
+++ tests/test_deferred.py
@@ -0,0 +1,19 @@
+import asyncio
+from twisted.trial.unittest import TestCase
+
+from scrapy_pyppeteer.download_handler import _force_deferred
+
+
+class ForceDeferredTestCase(TestCase):
+    def test_coroutine_to_deferred(self):
+        def _test(result):
+            self.assertIsInstance(result, str)
+            self.assertEqual(result, "Hello!")
+
+        async def say_hello():
+            await asyncio.sleep(0.01)
+            return "Hello!"
+
+        dfd = _force_deferred(say_hello())
+        dfd.addCallback(_test)
+        return dfd
