55e7a01f9a5450df9f2c9b7b9527e579e6384028
Eugenio Lacuesta <eugenio.lacuesta@gmail.com>
2020-05-20 16:51:05 -0300

    Bump version 0.0.9 -> 0.0.10

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="scrapy-pyppeteer",
-    version="0.0.9",
+    version="0.0.10",
     license="BSD",
     description="Pyppeteer integration for Scrapy",
     long_description=long_description,
